41d006eebe210b80d06dda32b061209bf6bc58d7
ematp <87378513+ematp@users.noreply.github.com>
2021-08-31 10:52:28 -0300

    GDevelop Fix

--- .git-script-update.sh
+++ .git-script-update.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "Test 2.0"
+
+cd "$HOME/.apps/gdevelop"
+original_name="$(ls | tail)"
+mv "$original_name" "gd5.AppImage"
+
+echo '[Desktop Entry]
+Version=1.0
+Type=Application
+Name=GDevelop 5.0
+Comment=
+Exec=/home/caraludme/.apps/gdevelop/gd5.AppImage -no-sandbox
+Icon=/home/caraludme/.local/share/icons/gdevelop.png
+Path=
+Terminal=false
+StartupNotify=true' > /home/caraludme/Escritorio/GDevelop.desktop
